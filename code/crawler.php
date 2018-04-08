7253eed2bf0568b37406d5055eba88c5d1aad74a
domisoch <37142494+domisoch@users.noreply.github.com>
2018-04-08 23:41:27 +0200

    Update crawler.php

--- code/crawler.php
+++ code/crawler.php
@@ -1,44 +1,94 @@
 <?php
-    $pageCrawlerResult = '';
+    include 'create_db.php';
+    
+    $pageCrawlerResult = [];
+    $crawlerDepth = 2;
+    $dbName = 'crawler';
+
+    //connection with database
+    $conn = new mysqli($servername, $username, $password, $dbname)
+    //check connection with database
+    if ($conn->connect_error) 
+    {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
 
     function crawl_page($url, $depth = 5)
     {
-        $result = '';
+        $hrefArray = [];
         static $seen = array();
-        if (isset($seen[$url]) || $depth === 0) {
+        if (isset($seen[$url]) || $depth === 0) 
+        {
             return;
         }
-
         $seen[$url] = true;
-
         $dom = new DOMDocument('1.0');
         @$dom->loadHTMLFile($url);
-
         $anchors = $dom->getElementsByTagName('a');
         $hrefArray = [];
-
-        foreach ($anchors as $element) {
-            $href = $element->getAttribute('href');
-
-            $hrefArray[] = $href;
+        foreach ($anchors as $element) 
+        {   // Remove anchors
+            $finalLink = explode("#", $element->getAttribute('href'));
+            $link = $finalLink[0];
+			// Add the protocol
+			$adres = substr($link, 0, 7);
+			$adresS = substr($link, 0, 8);
+			$protocol = 'http://';
+			$protocolS = 'https://';
+			if($adres != $protocol && $adresS != $protocolS)
+            {
+				echo '<br>Brak protokolu<br><br>';
+				$link = $url.$link;
+			}
+			// Push final link to array
+            $hrefArray[] = $link;
         }
         $hrefArray = array_unique($hrefArray);
-
-        foreach ($hrefArray as $href) {
-            $result .= '<a href="' . $href . '">' . $href . '</a>';
-        }
-
-        return $result;
+        return $hrefArray
     }
-    $url = $_GET['url'];
 
-    if(isset($url)) {
-        if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
+    if(!empty($_GET['url']))
+    {
+        $url = $_GET['url'];
+    }
+
+    if(isset($url)) 
+    {
+        if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) 
+        {
             echo 'Not a valid html!!!';
-        } else {
-            $pageCrawlerResult = crawl_page($url, 2);
+        } 
+        else 
+        {
+            $pageCrawlerResult = crawl_page($url, $crawlerDepth);
+            $pageContent = 'Page Content'
+            $sqlInsert = "INSERT INTO SitesAwaiting (site) VALUES ('$url')";
+            if (mysqli_query($conn, $sql)) 
+            {
+                echo "Query added successfully".'<br>';
+            } 
+            else 
+            {
+                echo "Error creating query " . mysqli_error($conn).'<br>';
+            }
+            //Links
+            foreach ($pageCrawlerResult as $link) 
+            {
+                $sql = "INSERT INTO SitesViewed (site, content) VALUES ('$url', '$link')";
+                if (mysqli_query($conn, $sql)) 
+                {
+                    //echo "Query added successfully".'<br>';
+                } 
+                else 
+                {
+                    echo "Error creating query " . mysqli_error($conn).'<br>';
+                }
+            }
         }
     }
+    // Close connection with database
+    $conn -> close();      
 ?>
 
 <!DOCTYPE html>
@@ -55,7 +105,7 @@
   	<div class="search">
         <form action="" type="GET">
             <div class="search-container">
-                <input type="text" class="search-input" name="url" value="<?php echo $url; ?>">
+                <input type="text" class="search-input" name="url" value="<?php if(!empty($_GET['url'])){ echo $url; } ?>">
             </div>
             <div class="submit-container">
                 <input class="submit" type="submit" value="Crawl!">
@@ -64,7 +114,10 @@
   	</div>
     <div class="result">
         <?php
-            echo $pageCrawlerResult;
+            foreach ($pageCrawlerResult as $href) 
+            {
+              echo '<a href = "'.$href.'">'.$href.'</a>';
+            }
         ?>
     </div>
   </body>
